73fd5a90e53bcd837d42c16fb47179fa45476944
nesquikx014 <n.eroglux014@gmail.com>
2024-12-13 10:41:45 +0100

    ik ben klaar  met classes 02

---
+++ Rijschool/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Rijschool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    class Auto {};
+    class Rijleraar {};
+    class Student {};
+    class LesPakket {};
+    class Rijtest {};
+    class Theorietest {};
+    class Dag {};
+    class LesUur {};
+}
